278f80b3660c76b9fa4e658fe6bb32627c2112b6
erikbonn <ebonn89@gmail.com>
2025-04-06 23:00:54 -0600

    feat: add line chart and table for market quick view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@
 
 import { SearchInput } from "@/components/ui/search-input";
 import { useRouter } from "next/navigation";
+import ScrollAnimation from "./_components/ScrollAnimation";
+import StickyFooter from "./_components/StickyFooter";
 
 export default function Home() {
   const router = useRouter();
@@ -14,19 +16,8 @@ export default function Home() {
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-gray-900 px-4">
-      <div className="text-center">
-        <h1 className="welcome-text text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Welcome to <span className="animated-gradient">Homeslice</span>
-        </h1>
-        <p className="mt-4 text-center text-xl text-orange-100">
-          Get to know your <strong>slice</strong> of the real estate market.
-        </p>
-        <div className="mt-8 flex w-full justify-center">
-          <div className="w-full max-w-2xl">
-            <SearchInput onSearch={handleSearch} />
-          </div>
-        </div>
-      </div>
+      <ScrollAnimation />
+      <StickyFooter />
     </div>
   );
 }
